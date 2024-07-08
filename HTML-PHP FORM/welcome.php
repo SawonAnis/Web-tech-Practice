a03cb96fd8b6dc06a3a0181225538832516c12c9
SawonAnis <anissawon44@gmail.com>
2024-07-08 13:20:26 +0600

    Form

---
+++ HTML-PHP FORM/welcome.php
@@ -0,0 +1,27 @@
+<html>
+
+<body>
+  <H3><b>GENERAL INFORMATION</b></H3>
+  <b>First Name :</b><?php echo $_POST["fname"]; ?><br>
+  <b>Last Name :</b> <?php echo $_POST["lname"];?> <br>
+  <b> Gender:</b><?php echo $_POST["gender"];?> <br>
+  <b>Father Name:</b><?php echo $_POST["faname"];?><br>
+  <b>Mother Name:</b><?php echo $_POST["mname"];?><br>
+  <b>Blood Group:</b><?php echo $_POST["BG"];?><br>
+
+  <h3><b>CONTACT INFORMATION</b></h3>
+  <b>Email:</b><?php echo $_POST["email"];?><br>
+  <b>Website
+  </b><?php echo $_POST["web"];?><br>
+  <b>Mobile:</b><?php echo $_POST["phone"];?><br>
+  <b>
+    Address:
+  </b><?php echo $_POST["adrs"];?>
+
+  <h3><b>ACCOUNT INFORMATION</b></h3>
+  <b>Username:</b><?php echo $_POST["uname"];?><br>
+  <b>Password:</b><?php echo $_POST["psw"];?>
+
+</body>
+
+</html>
